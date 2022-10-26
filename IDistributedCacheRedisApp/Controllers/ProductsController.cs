8cf8af45e802e27e8875a3eec0faf8acc25423e4
caner güreşci <caner.guresci@ibb.gov.tr>
2022-10-26 16:10:51 +0300

    Add project files.

---
+++ IDistributedCacheRedisApp/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using IDistributedCacheRedisApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace IDistributedCacheRedisApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEnry = new DistributedCacheEntryOptions();
+
+            cacheEnry.AbsoluteExpiration = DateTime.Now.AddMinutes(30);
+            Product product = new Product { Id = 3, Name = "kalem3", Price = 200 };
+
+            string jsonproduct = JsonConvert.SerializeObject(product); //json çevirme
+
+            Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct); //byte çevirme
+
+            _distributedCache.Set("product:3", byteproduct);
+
+
+            //await _distributedCache.SetStringAsync("product:2",jsonproduct,cacheEnry);
+
+            //_distributedCache.SetString("name", "caner");
+            //await _distributedCache.SetStringAsync("surname", "güreşci");
+            return View();
+        }
+
+        public async Task<IActionResult> Show()
+        {
+            //string name = _distributedCache.GetString("name");
+            //string surname = await _distributedCache.GetStringAsync("güreşci");
+            //ViewBag.name = name;
+            //ViewBag.surname = surname;
+
+            Byte[] byteProduct = _distributedCache.Get("product:3");
+
+            string jsonproducts = Encoding.UTF8.GetString(byteProduct);
+            
+            //string jsonproducts = _distributedCache.GetString("product:1");
+
+            Product p = JsonConvert.DeserializeObject<Product>(jsonproducts);
+
+            ViewBag.product = p;
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> Remove()
+        {
+            _distributedCache.Remove("name");
+            await _distributedCache.RemoveAsync("surname");
+            return View();
+        }
+
+        public IActionResult ImageUrl()
+        {
+            byte[] resimbyte = _distributedCache.Get("resim");
+
+            return File(resimbyte, "image/jpg");
+        }
+
+
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/download.jpg"); //resimi yolunu verdik
+            byte[] imageBye = System.IO.File.ReadAllBytes(path); //daha sonra byte çevirdik
+
+            _distributedCache.Set("resim", imageBye); //image dosyasını binary olarak kaydettik.
+            return View();
+        }
+    }
+}
---
+++ InMemory/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using InMemory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly IMemoryCache _cache;
+
+        public ProductController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+
+            //1.yol
+            //if (String.IsNullOrEmpty(_cache.Get<string>("zaman")))
+            //{
+            //    _cache.Set<string>("zaman", DateTime.Now.ToString());
+
+            //}
+            //out keyword bir mettota birden fazla değer dönebilmek için.
+            //2.yol
+            //if (!_cache.TryGetValue("zaman", out string zamancache))
+            //{
+            //} //bu method ile gelen zaman keyini al ve zamancache ata dedik 
+
+
+            MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10); //adminites
+            //options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            options.Priority = CacheItemPriority.High;
+            options.RegisterPostEvictionCallback((key, value, reason, state) => //bir data silindiği zaman artık hangi keyivalue ve sebep ile silindiğini görebiliriz.
+            {
+                _cache.Set("callback", $"{key} -->{value} => sebep:{reason}");
+            });
+            _cache.Set<string>("zaman", DateTime.Now.ToString(), options);
+            Product p = new Product { Id = 1, Name = "kalem", Price=200 };
+            _cache.Set<Product>("product:1",p);
+
+            return View();
+        }
+        //AbsoluteExpiration bu method cache verdiğimiz zamanı alır. Cach oluşturulduğunda 5 dk verirsek 5 dk sonracahce siler
+
+        //SlidingExpiration 3 dk verdik süre oalrak 3 dk içinde dataya erişemez ise silinir eğerki ulaşırsa o zaman 3 dk uzatır. slidenn verilince mutlaka absolute verilir. yokise bayat veriye ulaşam durumu olur.
+        public IActionResult Show()
+        {
+            //bu method ile zaman diye bir kyword varmı diye kontrol et yok ise git o zaman aç ve kaydet dedik. bu kullanımın amacıda entry.diyip bunun özeeliklerini vermek içinç
+            //_cache.GetOrCreate<string>("zaman", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
+
+            _cache.TryGetValue("zaman", out string zamancache);
+            _cache.TryGetValue("callback", out string callback);
+            ViewBag.zaman = zamancache;
+            ViewBag.callback = callback;
+            ViewBag.product = _cache.Get<Product>("product:1");
+            /*ViewBag.zaman = _cache.Get<string>("zaman")*/
+            ; //Viewbag ile veriyi view gönderdik
+            return View();
+
+        }
+
+    }
+}
